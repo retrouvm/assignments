c30f90f085cc0b6a463ffde78cac93d17cc5c81e
retrouvm <retrouvaillesm@gmail.com>
2023-09-16 17:35:13 -0500

    assign1

---
+++ Operating_Systems/assignments/assign1/assign1/multiple_wordcount.c
@@ -0,0 +1,25 @@
+/*
+ * File: multiple_wordcount.c
+ * YOUR NAME ... YOU NEED TO IMPLEMENT THE FUNCTIONS HERE....
+ *
+ * ....
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+// include other standard header files or your own user defined libraries needed 
+
+ 
+
+/*
+ * YOUR COMMENTS
+ */
+
+
+int main(int argc, char *arvg[])
+{
+  // YOU NEED TO IMPLEMENT THIS + some other functions if needed! 
+ 
+  return 0;
+}
+   
---
+++ Operating_Systems/assignments/assign1/assign1/wordcount.c
@@ -0,0 +1,25 @@
+/*
+ * File: wordcount.c
+ * YOUR NAME ... YOU NEED TO IMPLEMENT THE FUNCTIONS HERE....
+ *
+ * ....
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+// include other standard header files or your own user defined libraries needed 
+
+ 
+
+/*
+ * YOUR COMMENTS
+ */
+
+
+int main(int argc, char *arvg[])
+{
+  // YOU NEED TO IMPLEMENT THIS + some other functions! 
+ 
+  return 0;
+}
+   
